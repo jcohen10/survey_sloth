8547401aba9de53bd3196a4af99c9597a8340868
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2014-07-04 21:08:43 -0400

    retweaked Jquery for dynamic form

--- public/js/application.js
+++ public/js/application.js
@@ -3,28 +3,25 @@ $(document).ready(function() {
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+var addDiv = $('#items');
 
-//create three initial fields
-var startingNo = 3;
-var $node = "";
-for(varCount=0;varCount<=startingNo;varCount++){
-    var displayCount = varCount+1;
-    $node += '<p><label for="var'+displayCount+'">Variable '+displayCount+': </label><input type="text" name="var'+displayCount+'" id="var'+displayCount+'"><span class="removeVar">Remove Variable</span></p>';
-}
-//add them to the DOM
-$('#survey').prepend($node);
-//remove a textfield
-$('form').on('click', '.removeVar', function(){
-   $(this).parent().remove();
+var i = $('#items').size() + 1;
+console.log(i);
+console.log($('#items').size())
 
-});
-//add a new node
-$('#addVar').on('click', function(){
-varCount++;
-$node = '<p><label for="var'+varCount+'">Variable '+varCount+': </label><input type="text" name="var'+varCount+'" id="var'+varCount+'"><span class="removeVar">Remove Variable</span></p>';
-$(this).parent().before($node);
-});
+  $('#add').click(function(e) {
+    $('<p><input type="text" id="new_choice" placeholder = "New Possible Answer" name="choice '+ i +'"><button id="remove_choice">Remove!</button></p>').appendTo(addDiv);
+    i++;
+    return false; //Investigate this.
+  });
 
+  $('#remove_choice').bind('click', function(e){
+    if (i > 2) {
+      console.log('yolo')
+      // $(this).parents.remove();
+      i--;
+      console.log(i)
+    }
+  });
 
 });
